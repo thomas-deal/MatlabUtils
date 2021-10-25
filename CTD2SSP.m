3f021820ed0333c8b0abb48b3236c38b7018929d
Tom Deal <thomas.deal@gmail.com>
2021-10-25 11:27:07 -0400

    Move scripts to ocean volume modeling repository.

--- CTD2SSP.m
+++
@@ -1,7 +0,0 @@
-function c = CTD2SSP(depth,salinity,temp)
-
-%% Calculate
-% Medwin 1975
-c = 1449.2 + 4.6*temp - 5.5e-2*temp.^2 + 2.9e-4*temp.^3 + ...
-    (1.34 - 1e-2*temp).*(salinity - 35) + ...
-    1.6e-2*depth;
