eb04c9e1ee7c71e9c8ec893208912c2b4e787294
Tom Deal <tjdeal1@nps.edu>
2021-12-20 06:43:53 -0500

    Add 3xN input support for RotationMatrix

--- RotationMatrix.m
+++ RotationMatrix.m
@@ -1,16 +1,20 @@
-function R = RotationMatrix(gamma,theta,psi)
+function R = RotationMatrix(varargin)
 %% function R = RotationMatrix(gamma,theta,psi)
+% function R = RotationMatrix(Ori)
 %
 % Creates a rotation matrix for converting Cartesian coordinates from one
 % frame to another frame rotated gamma degrees about the x axis, theta
 % degrees about the y axis, and psi degrees about the z axis in a
 % right-handed coordinate frame. If gamma, theta, and psi are vectors of
-% the same length N, R is of dimension 3x3xN.
+% the same length N, R is of dimension 3x3xN. Instead of passing gamma,
+% theta, and psi individually, they can bbe combined as rows in the 3xN
+% mareix Ori.
 %
 % Inputs:
 %           gamma   - Roll angle, deg
 %           theta   - Pitch or elevation angle, deg
 %           psi     - Yaw or azimuthal angle, deg
+%           Ori     - [gamma;theta;psi] passed as column vector or matrix
 %
 % Outputs:
 %           R       - Rotation matrix
@@ -19,19 +23,40 @@ function R = RotationMatrix(gamma,theta,psi)
 %% Initialize
 R = eye(3);
 %% Check Input Dimensions
-[rg,cg] = size(gamma);
-[rt,ct] = size(theta);
-[rp,cp] = size(psi);
-if ~(rg==rt&&rt==rp&&cg==ct&&ct==cp)
-    disp('RotationMatrix: inputs gamma, theta, and psi have incompatible dimensions')
-    return
-end
-if min([rg cg])~=1
-    disp('RotationMatrix: inputs gamma, theta, and psi must be scalars or vectors, not matrices')
-    return
+switch nargin
+    case 1
+        Ori = varargin{1};
+        if size(Ori,1)~=3
+            disp('RotationMatrix: input Ori has incorrect dimensions')
+            return
+        else
+            gamma = Ori(1,:);
+            theta = Ori(2,:);
+            psi   = Ori(3,:);
+        end
+    case 3
+        gamma = varargin{1};
+        theta = varargin{2};
+        psi   = varargin{3};
+        [rg,cg] = size(gamma);
+        [rt,ct] = size(theta);
+        [rp,cp] = size(psi);
+        if ~(rg==rt&&rt==rp&&cg==ct&&ct==cp)
+            disp(['RotationMatrix: inputs gamma, theta, and psi ' ...
+                  'have incompatible dimensions'])
+            return
+        end
+        if min([rg cg])~=1
+            disp(['RotationMatrix: inputs gamma, theta, and psi ' ...
+                  'must be scalars or vectors, not matrices'])
+            return
+        end
+    otherwise
+        disp('RotationMatrix: incorrect number of input arguments')
+        return
 end
 %% Calculate
-N = max([rg,cg]);
+N = length(gamma);
 R = zeros(3,3,N);
 for i=1:N
     Rx = [1 0 0; ...
